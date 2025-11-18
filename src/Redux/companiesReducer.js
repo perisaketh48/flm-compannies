4ef60aa573e9afc472ecf50454fc4d12205c58be
saketh.peri@nexiilabs.com <perisaketh545@gmail.com>
2025-11-18 23:02:38 +0530

    change 2.0

--- src/Redux/companiesReducer.js
+++ src/Redux/companiesReducer.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const initialState = {
   items: [],
